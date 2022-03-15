61761459536c3614fbe6d6f576549afff8a02fa9
Vo Huy Nguyen <huynguyenvo2001@gmail.com>
2022-03-15 23:36:03 +0700

    create pages and navigation links

--- carpages/views.py
+++ carpages/views.py
@@ -3,3 +3,12 @@ from django.shortcuts import render
 # Create your views here.
 def home(request):
     return render(request, 'carpages/home.html')
+
+def about(request):
+    return render(request, 'carpages/about.html')
+
+def services(request):
+    return render(request, 'carpages/services.html')
+
+def contact(request):
+    return render(request, 'carpages/contact.html')
--- carzone/settings.py
+++ carzone/settings.py
@@ -9,7 +9,7 @@ https://docs.djangoproject.com/en/4.0/topics/settings/
 For the full list of settings and their values, see
 https://docs.djangoproject.com/en/4.0/ref/settings/
 """
-
+import os
 from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
@@ -75,12 +75,19 @@ WSGI_APPLICATION = 'carzone.wsgi.application'
 # https://docs.djangoproject.com/en/4.0/ref/settings/#databases
 
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+     'default': {
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'cardb',
+        'USER': 'root',
+        'PASSWORD': '12345678',
+        'HOST': '' # mặc định localhost
     }
 }
 
+import pymysql
+pymysql.install_as_MySQLdb()
+
+AUTH_USER_MODEL = 'carpages.User'
 
 # Password validation
 # https://docs.djangoproject.com/en/4.0/ref/settings/#auth-password-validators
@@ -117,6 +124,10 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.0/howto/static-files/
 
 STATIC_URL = 'static/'
+STATIC_ROOT = os.path.join(BASE_DIR, 'static')
+STATICFILES_DIRS = [
+    os.path.join(BASE_DIR, 'carpages/static'),
+]
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.0/ref/settings/#default-auto-field
